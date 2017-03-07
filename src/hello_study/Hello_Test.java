c11334fa2339f71bc4db57b0a915b46f6a36dbef
osram21 <osram21@naver.com>
2017-03-07 10:41:19 +0900

    Update Hello_Test.java
    마지막추가

--- src/hello_study/Hello_Test.java
+++ src/hello_study/Hello_Test.java
@@ -4,6 +4,7 @@ public class Hello_Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
 		System.out.println("Hello Two Test");
-		System.out.printlnn("Git Hub Edit");
+		System.out.println("Git Hub Edit");
+		System.out.println("List add");
 	}
 }
