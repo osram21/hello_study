a23511adfad1e688ed9c015e5670d4104dffe011
osram21 <osram21@naver.com>
2017-03-07 10:31:39 +0900

    Update Hello_Test.java
    깃허브에서 수정

--- src/hello_study/Hello_Test.java
+++ src/hello_study/Hello_Test.java
@@ -4,5 +4,6 @@ public class Hello_Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
 		System.out.println("Hello Two Test");
+		System.out.printlnn("Git Hub Edit");
 	}
 }
