03a1e3ec39650eb4e3f783e3a2180b11e8c7e784
osram21 <osram21@naver.com>
2017-03-07 10:32:46 +0900

    로컬에서 추가

--- src/hello_study/Hello_Test.java
+++ src/hello_study/Hello_Test.java
@@ -4,5 +4,6 @@ public class Hello_Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
 		System.out.println("Hello Two Test");
+		System.out.println("로컬에서 추가");
 	}
 }
